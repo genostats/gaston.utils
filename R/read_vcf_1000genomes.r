9cec33f8d82e8378cea847ffc5723003fa6bdb52
HervePerdry <herve.perdry@gmail.com>
2018-05-14 10:13:32 +0200

    modif read 1000 genomes

--- R/read_vcf_1000genomes.r
+++ R/read_vcf_1000genomes.r
@@ -3,10 +3,15 @@ read.vcf.1000genomes <- function(dir, x, by = "chr:pos:alleles", samples, popula
   # pour l'instant on ne lit que les autosomes !! (c'est le bordel avec le chr Y)
   filename <- sprintf("%s/ALL.chr%d.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz", path.expand(dir), 1:22)
 
-  if(class(x) == "bed.matrix")
-    x <- x@snps
-  if(class(x) != "data.frame")
-    stop("x should be a bed.matrix or a data frames")
+  if(!missing(x)) {
+    filter.snps <- TRUE
+    if(class(x) == "bed.matrix")
+      x <- x@snps
+    if(class(x) != "data.frame")
+      stop("x should be a bed.matrix or a data frames")
+  } else {
+    filter.snps <- FALSE
+  }
 
   # pour 1000 genomes le défaut doit être ok
   set.chr.ids() 
@@ -20,12 +25,16 @@ read.vcf.1000genomes <- function(dir, x, by = "chr:pos:alleles", samples, popula
       samples <- character(0)
   }
 
-  if(by == "chr:pos") 
-    L <- .Call("gg_read_vcf_chr_pos", PACKAGE = "gaston.utils", filename, get.info, x$chr, x$pos, samples)
-  else if(by == "chr:pos:alleles")
-    L <- .Call("gg_read_vcf_chr_pos_al", PACKAGE = "gaston.utils", filename, get.info, x$chr, x$pos, x$A1, x$A2, samples)
-  else
-    stop("Parameter 'by' should be 'chr:pos' or 'chr:pos:alleles'")
+  if(filter_snps) {
+    if(by == "chr:pos") 
+      L <- .Call("gg_read_vcf_chr_pos", PACKAGE = "gaston.utils", filename, get.info, x$chr, x$pos, samples)
+    else if(by == "chr:pos:alleles")
+      L <- .Call("gg_read_vcf_chr_pos_al", PACKAGE = "gaston.utils", filename, get.info, x$chr, x$pos, x$A1, x$A2, samples)
+    else
+      stop("Parameter 'by' should be 'chr:pos' or 'chr:pos:alleles'")
+   } else {
+   L <- .Call("gg_read_vcf_chr_range", PACKAGE = "gaston.utils", filename, get.info, -1L, 0L, 0L, samples)
+  }
 
   snp <- data.frame(chr = L$chr, id = L$id, dist = rep(0, length(L$chr)), pos = L$pos , A1 = L$A1, A2 = L$A2,
                     quality = L$quality, filter = factor(L$filter), stringsAsFactors = FALSE)
