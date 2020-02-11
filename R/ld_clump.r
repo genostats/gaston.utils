db9941f36a9347a7efbb23a7b0fafab56c3ef87e
HervePerdry <herve.perdry@gmail.com>
2020-02-11 12:58:15 +0100

    ld_clump: improved the returned data frame

--- R/ld_clump.r
+++ R/ld_clump.r
@@ -5,14 +5,24 @@ LD.clump <- function(x, p, r2.threshold, p.threshold, max.dist = 500e3) {
   if(is.data.frame(p)) {
     if(!all(p$chr == x@ped$chr, na.rm = TRUE) | !all(p$pos == x@ped$pos, na.rm = TRUE))
       stop("Unmatching SNPs")
+    a <- p
     p <- p$p
-  } 
+  } else {
+    a <- NULL
+  }
   if(length(p) != ncol(x))
     stop("Dimensions mismatch")
 
   or <- order(p) - 1 # -1 pour des indices qui démarrent à 0 ds la fction c++
   I <- .Call("ld_clump", PACKAGE = "gaston.utils", x@bed, x@mu, x@sigma, r2.threshold, x@snps$pos, x@snps$chr, max.dist, or);
-  data.frame( chr = x@snps$chr, id = x@snps$id, pos = x@snps$pos, p = p, index = I+1)
+
+  if(is.null(a))
+    a <- data.frame( chr = x@snps$chr, id = x@snps$id, pos = x@snps$pos, p = p, cluster = I+1)
+  else {
+    a$cluster <- I+1
+  }
+  a$is.index <- (a$cluster == 1:nrow(a))
+  a
 }
 
 
